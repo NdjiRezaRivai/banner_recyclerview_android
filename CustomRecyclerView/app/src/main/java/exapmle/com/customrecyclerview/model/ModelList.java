4fb54251c5df534e383ae6e04f5f2c8907bf6786
Ndji Reza Rivai <rezandji@gmail.com>
2018-05-05 14:19:00 +0700

    Revert "Revert "Playstore Animation""
    This reverts commit c8be82461a8997f17d619b1c90d6173071e668f7.

---
+++ CustomRecyclerView/app/src/main/java/exapmle/com/customrecyclerview/model/ModelList.java
@@ -0,0 +1,36 @@
+package exapmle.com.customrecyclerview.model;
+
+/**
+ * Created by GITAcerV3 on 5/5/2018.
+ */
+
+public class ModelList {
+
+    private String title;
+    private int img;
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
